df4acdf48f2d91aa7c1f3158faf3c477b2a71f5f
yeinnymelissa <3017261750101@ingenieria.usac.edu.gt>
2023-02-05 20:51:44 -0600

    Ultimos arreglos

--- login/src/main/java/com/practica2/login/jwt/JwtFilter.java
+++ login/src/main/java/com/practica2/login/jwt/JwtFilter.java
@@ -21,15 +21,10 @@ public class JwtFilter extends GenericFilterBean{
 		final HttpServletRequest request = (HttpServletRequest) servletRequest;
         final HttpServletResponse response = (HttpServletResponse) servletResponse;
         final String authHeader = request.getHeader("authorization");
-        if ("OPTIONS".equals(request.getMethod())) {
-            response.setStatus(HttpServletResponse.SC_OK);
-            filterChain.doFilter(request, response);
-        } 
         try {
 	        final String token = authHeader.substring(7);
 	        Claims claims = Jwts.parser().setSigningKey("secret").parseClaimsJws(token).getBody();
 	        request.setAttribute("claims", claims);
-	        request.setAttribute("blog", servletRequest.getParameter("id"));
 	        filterChain.doFilter(request, response);
         }catch(Exception e) {
         	System.out.println("Ocurrio un error");
